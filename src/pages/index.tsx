5127a65f9e124bf535b9383c51126dfe7c9cee9d
Archna-1 <archnas2@illinois.edu>
2023-02-09 17:37:02 -0600

    Finish character grid
    Co-authored-by: Rustom Ichhaporia <rustom@rustom.dev>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,36 @@
-import { Center, Heading, VStack } from '@chakra-ui/react';
+import { Card, Center, Heading, VStack, CardBody, Image, Stack, Grid } from '@chakra-ui/react';
 import { type NextPage } from 'next';
 
 import { api } from '../utils/api';
 
 const Home: NextPage = () => {
-  const hello = api.characterRouter.hello.useQuery({ name: 'Meraki' });
-  const count = api.characterRouter.count.useQuery();
+  const characters = api.characterRouter.getCharacters.useQuery()?.data;
 
   return (
     <Center minH={'100vh'}>
       <VStack spacing={5}>
-        <Heading as="h1" size="2xl">
-          {hello.data?.greeting}
-        </Heading>
-        <Heading as="h2" size="md">
-          There are {count?.data ?? 0} characters in the DB.
-        </Heading>
+        <Grid maxWidth='80%' templateColumns='repeat(3, 5fr)' gap={5}>
+          {characters && characters.map((character) => {
+            return (
+              <a href={`/character/${character.id}`} key={character.name}>
+                <Card width='100%' borderRadius={0}>
+                  <CardBody width='100%' margin={0} padding={0}>
+                    <Image
+                      height={200}
+                      width={300}
+                      objectFit='cover'
+                      src={character.imageUrl}
+                      alt={character.name}
+                    />
+                    <Center _hover={{ bg: 'yellow.400', color: 'black' }}>
+                      <Heading size='md' margin={5} > {character.name} </Heading>
+                    </Center>
+                  </CardBody>
+                </Card>
+              </a>
+            )
+          })}
+        </Grid>
       </VStack>
     </Center>
   );
