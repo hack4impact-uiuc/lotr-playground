47d6831bb8056315650d5e4e0f8e26f0df3982ff
Vishruth Raj <vishraj248@gmail.com>
2023-02-09 07:58:51 -0600

    Start working on UI

--- src/pages/character/[id].tsx
+++ src/pages/character/[id].tsx
@@ -2,6 +2,17 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { api } from '../../utils/api';
 
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Text,
+  Button,
+} from '@chakra-ui/react';
+
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -17,20 +28,59 @@ const Post = () => {
   const characterInfo = characterRequest.data;
 
   return (
-    <>
-      <p>{characterInfo?.name}</p>
-      <p>{characterInfo?.gender}</p>
-      <p>{characterInfo?.race}</p>
-      <p>{characterInfo?.realm}</p>
-      {characterInfo && (
-        <Image
-          src={characterInfo.imageUrl}
-          alt={`Picture of ${characterInfo.name}`}
-          width={300}
-          height={400}
-        />
-      )}
-    </>
+    <div style={{ display: 'flex' }}>
+      <Card
+        direction={{ base: 'column', sm: 'row' }}
+        overflow="hidden"
+        variant="outline"
+        bg="black"
+        borderRadius="0"
+      >
+        <Stack>
+          <CardBody>
+            <Heading size="md" color="white">
+              {characterInfo?.name}
+            </Heading>
+
+            <Text py="2" color="white">
+              General Information
+            </Text>
+          </CardBody>
+
+          <Card
+            direction={{ base: 'column' }}
+            overflow="hidden"
+            variant="outline"
+            bg="gray"
+            borderRadius="0"
+          >
+            <Text py="1" color="white">
+              Gender: {characterInfo?.gender}
+            </Text>
+            <Text py="1" color="white">
+              Race: {characterInfo?.race}
+            </Text>
+            <Text py="1" color="white">
+              Realm: {characterInfo?.realm}
+            </Text>
+          </Card>
+
+          <CardFooter>
+            <Button variant="solid" colorScheme="yellow" borderRadius="0">
+              Edit Character
+            </Button>
+          </CardFooter>
+        </Stack>
+        {characterInfo && (
+          <Image
+            src={characterInfo.imageUrl}
+            alt={`Picture of ${characterInfo.name}`}
+            width={300}
+            height={400}
+          />
+        )}
+      </Card>
+    </div>
   );
 };
 
