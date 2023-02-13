339c1b737005eaf7c49ac81b65c43d2330a9daa1
Rustom Ichhaporia <rustom@rustom.dev>
2023-02-13 10:24:59 -0600

    Create demo character grid (#10)
    * Finish character grid
    
    Co-authored-by: Rustom Ichhaporia <rustom@rustom.dev>
    
    * Format code
    
    * Ignore settings.json
    
    * Fix color typing
    
    * Formatting
    
    Co-authored-by: Archna Sobti <Archna-1@users.noreply.github.com>
    
    * Add chakra abbreviations and remove extra space
    
    ---------
    
    Co-authored-by: Archna-1 <archnas2@illinois.edu>
    Co-authored-by: Archna Sobti <Archna-1@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,51 @@
-import { Center, Heading, VStack } from '@chakra-ui/react';
+import {
+  Card,
+  Center,
+  Heading,
+  VStack,
+  CardBody,
+  Image,
+  Grid,
+} from '@chakra-ui/react';
 import { type NextPage } from 'next';
 
 import { api } from '../utils/api';
 
 const Home: NextPage = () => {
-  const hello = api.characterRouter.hello.useQuery({ name: 'Meraki' });
-  const count = api.characterRouter.count.useQuery();
+  const characters = api.characterRouter.getCharacters.useQuery()?.data;
 
   return (
-    <Center minH={'100vh'}>
+    <Center minH={'100vh'} backgroundColor="gray.900">
       <VStack spacing={5}>
-        <Heading as="h1" size="2xl">
-          {hello.data?.greeting}
-        </Heading>
-        <Heading as="h2" size="md">
-          There are {count?.data ?? 0} characters in the DB.
-        </Heading>
+        <Grid maxW="80%" templateColumns="repeat(3, 5fr)" gap={5}>
+          {characters &&
+            characters.map((character) => {
+              return (
+                <a href={`/character/${character.id}`} key={character.name}>
+                  <Card w="100%" borderRadius={0}>
+                    <CardBody w="100%" m={0} p={0}>
+                      <Image
+                        h={200}
+                        w={300}
+                        objectFit="cover"
+                        src={character.imageUrl}
+                        alt={character.name}
+                      />
+                      <Center
+                        bgColor="background"
+                        color="white"
+                        _hover={{ bg: 'gold', color: 'gray.900' }}
+                      >
+                        <Heading size="md" m={5} fontFamily="heading">
+                          {character.name}
+                        </Heading>
+                      </Center>
+                    </CardBody>
+                  </Card>
+                </a>
+              );
+            })}
+        </Grid>
       </VStack>
     </Center>
   );
