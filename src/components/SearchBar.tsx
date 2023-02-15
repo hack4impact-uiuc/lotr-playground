a5cec2ccb3d53fc5694d32412efe60cd86985eb1
Rustom Ichhaporia <rustom@rustom.dev>
2023-02-14 20:58:29 -0600

    Make huge progress on the front 🔚 (#16)
    * Do the issue
    
    * Make fixes
    
    * Consolidate Rustom's Add Character button with Khush/Vy modal

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,21 @@
+import { SearchIcon } from '@chakra-ui/icons';
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+
+const SearchBar = () => {
+  return (
+    <InputGroup>
+      <InputLeftElement pointerEvents="none">
+        <SearchIcon color="gray.300" />
+      </InputLeftElement>
+      <Input
+        type="text"
+        w="100%"
+        borderRadius={0}
+        bg="background"
+        color="white"
+      />
+    </InputGroup>
+  );
+};
+
+export default SearchBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,56 @@
 import {
+  Box,
   Card,
   CardBody,
   Center,
+  Flex,
   Grid,
   Heading,
   Image,
   VStack,
 } from '@chakra-ui/react';
 import { type NextPage } from 'next';
-import AddCharacterForm from '../components/AddCharacterForm';
 
+import AddCharacterForm from '../components/AddCharacterForm';
+import SearchBar from '../components/SearchBar';
 import { api } from '../utils/api';
 
 const Home: NextPage = () => {
   const characters = api.characterRouter.getCharacters.useQuery()?.data;
 
   return (
-    <Center minH="100vh" backgroundColor="gray.900">
-      <VStack spacing={5}>
-        <AddCharacterForm />
-        <Grid maxW="80%" templateColumns="repeat(3, 5fr)" gap={5}>
+    <Center minH="100vh" bgColor="#131616">
+      <Image
+        top={0}
+        linear-gradient="(135deg, #50A68480 30%, #115E6780 90%)"
+        w="100%"
+        position="absolute"
+        src="/wallpaperflare-cropped 1.png"
+        alt="Lord of the Rings Wallpaper"
+      />
+      <Box
+        w="100%"
+        h="700px"
+        position="absolute"
+        top="247px"
+        bg="linear-gradient(180deg, rgba(10, 13, 14, 0) 0%, #131617 31.77%)"
+      />
+      <VStack spacing={5} w="45%" pt="2%" pb="5%">
+        <Image
+          position="relative"
+          w="45%"
+          src="/lord-of-the-rings-png-title-logo-1.png"
+          alt="Lord of the Rings Logo"
+        />
+        <Flex direction="row" justify="flex-end" w="100%">
+          {/* <Button bgColor="gold" color="black" borderRadius={0}>
+            Add Character
+          </Button> */}
+          <AddCharacterForm />
+
+        </Flex>
+        <SearchBar />
+        <Grid templateColumns="repeat(3, 5fr)" gap={5}>
           {characters &&
             characters.map((character) => {
               return (
